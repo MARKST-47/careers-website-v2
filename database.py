583a4ce4d5a0728290d13bd41404009864f10cc8
MARKST-47 <71715278+MARKST-47@users.noreply.github.com>
2024-01-07 13:28:38 +0000

    Added secret keys

--- database.py
+++ database.py
@@ -1,28 +1,32 @@
 import pymysql
+import os
+
+db_password = os.environ['DB_PASSWORD']
+db_host = os.environ['DB_HOST']
 
 timeout = 10
 connection = pymysql.connect(
-  charset="utf8mb4",
-  connect_timeout=timeout,
-  cursorclass=pymysql.cursors.DictCursor,
-  db="defaultdb",
-  host="mst-careers-mst-careers.a.aivencloud.com",
-  password="AVNS_YpbojKNuIGzBgdfn43L",
-  read_timeout=timeout,
-  port=17333,
-  user="avnadmin",
-  write_timeout=timeout,
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db="defaultdb",
+    host=db_host,
+    password=db_password,
+    read_timeout=timeout,
+    port=17333,
+    user="avnadmin",
+    write_timeout=timeout,
 )
+
 
 def load_jobs_from_db():
   jobs = []
   try:
     with connection.cursor() as cursor:
-      cursor = connection.cursor()
       cursor.execute("SELECT * FROM jobs")
       for row in cursor.fetchall():
-          jobs.append(row)
+        jobs.append(row)
   except Exception as e:
-      # Handle the exception (print, log, or raise if    necessary)
-      print(f"Error: {e}")
+    # Handle the exception (print, log, or raise if    necessary)
+    print(f"Error: {e}")
   return jobs
